f16c2d1fe4a85d71a9cf3021ce756e2472c6b261
Jon Staff <jstaff@indatus.com>
2014-06-12 10:07:44 -0400

    Releasing the connection in Util

--- src/com/jonathonstaff/ideaascii/util/Util.java
+++ src/com/jonathonstaff/ideaascii/util/Util.java
@@ -1,6 +1,6 @@
 package com.jonathonstaff.ideaascii.util;
-//  Created by jonstaff on 6/11/14.
 
+//  Created by jonstaff on 6/11/14.
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -17,7 +17,8 @@ public class Util {
 
     public static String convertTextToAscii(String text, String font) {
         HttpClient client = new HttpClient();
-        GetMethod get = new GetMethod("http://artii.herokuapp.com/make?text=" + urlEncodedString(text) + "&font=" + font);
+        GetMethod get = new GetMethod("http://artii.herokuapp.com/make?text="
+                + urlEncodedString(text) + "&font=" + font);
 
         String response = "Something went wrong...";
         try {
@@ -25,6 +26,8 @@ public class Util {
             response = get.getResponseBodyAsString();
         } catch (IOException e1) {
             e1.printStackTrace();
+        } finally {
+            get.releaseConnection();
         }
 
         return response;
